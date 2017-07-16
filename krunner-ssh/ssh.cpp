0d324f77999be81961af1a19d1db64abb0152d30
Björn Bidar <theodorstormgrade@gmail.com>
2017-07-17 00:36:05 +0200

    use kde default terminal app rather than just use konsole

--- krunner-ssh/ssh.cpp
+++ krunner-ssh/ssh.cpp
@@ -31,6 +31,9 @@
 #include <KLocalizedString>
 #include <krun.h>
 #include <KShell>
+#include <KConfig>
+#include <KConfigGroup>
+#include <KSharedConfig>
 
 #include <iostream>
 
@@ -212,7 +215,10 @@ void SSHRunner::run(const Plasma::RunnerContext &context, const Plasma::QueryMat
 
 	QString command = QString("ssh %1").arg(host);
 
-	QString konsole_command = QString("konsole -e %1").arg(command);
+
+        KConfigGroup config(KSharedConfig::openConfig(QStringLiteral("kdeglobals")), "General");
+        QString terminal = config.readPathEntry("TerminalApplication",QStringLiteral("konsole"));
+	QString konsole_command = QString(terminal+" -e %1").arg(command);
 
 	KRun::runCommand(konsole_command, 0);
 }
